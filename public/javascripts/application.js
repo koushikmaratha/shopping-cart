7211ead18546087bdaa5ede4589d35de0c08cc16
dylankb <dylan.barnard@gmail.com>
2018-02-05 11:04:02 -0800

    Add product data, fix loading error

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
-class Application extends React.Component {
+class ShoppingManager extends React.Component {
   render() {
     return (
     <div>
@@ -39,11 +39,6 @@ class Product extends React.Component {
         );
     }
 }
-
-
-
-
-
 
 ReactDOM.render(
   <ShoppingManager />,
