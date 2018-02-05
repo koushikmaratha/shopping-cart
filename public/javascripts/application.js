34fb36a038476066a0d9f7b0f612f26af42b4705
dylankb <dylan.barnard@gmail.com>
2018-02-05 11:32:32 -0800

    Fix spacing, add cart components

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,35 +10,70 @@ class ShoppingManager extends React.Component {
 }
 
 class ProductManager extends React.Component {
-    render () {
-        return (
-            <ProductList product_data={this.props.product_data}/>
-        );
-    }
+  render () {
+    return (
+      <ProductList product_data={this.props.product_data}/>
+    );
+  }
 }
 
 class ProductList extends React.Component {
-    render () {
-        let products = this.props.product_data.map((product, index) => {
-                        return <Product key={product.title + '-' + index}title={product.title} price={product.price} quantity={product.quantity} />
-                      });
-        return (
-          <div>
-            {products}
-          </div>
-        );
-    }
+  render () {
+    let products = this.props.product_data.map((product, index) => {
+                    return <Product key={product.title + '-' + index}title={product.title} price={product.price} quantity={product.quantity} />
+                   });
+    return (
+      <div>
+        {products}
+      </div>
+    );
+  }
 }
 
 class Product extends React.Component {
-    render () {
-        return (
-            <div className="product">
-                <p>{this.props.title} - {this.props.price} x {this.props.quantity}</p>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div className="product">
+        <p>{this.props.title} - {this.props.price} x {this.props.quantity}</p>
+      </div>
+    );
+  }
 }
+
+class CartManager extends React.Component {
+  render() {
+    return (
+      <CartItemList />
+    );
+  }
+}
+
+class CartItemList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Title</h3>
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+    );
+  }
+}
+
+class CartItem extends React.Component {
+  render() {
+    return (
+      <div className="cart">
+        <p>{this.props.title} - {this.props.price}</p>
+      </div>
+    );
+  }
+}
+
+
+
+
 
 ReactDOM.render(
   <ShoppingManager />,
