cfae40a21629274ecac87c07c263830ee7720e4e
Tannr Allard <tallard@localhost.localdomain>
2018-02-05 13:56:49 -0500

    First commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,51 @@
 class Application extends React.Component {
   render() {
     return (
+    <div>
       <p>Welcome to the Shop!</p>
+      <ProductManager />
+    </div>
     );
   }
 }
 
+class ProductManager extends React.Component {
+    render () {
+        return (
+            <ProductList />
+        );
+    }
+}
+
+class ProductList extends React.Component {
+    render () {
+        return (
+        <div>
+            <Product />
+            <Product />
+            <Product />
+        </div>
+        );
+    }
+}
+
+class Product extends React.Component {
+    render () {
+        return (
+            <div>
+                <p>{this.props.title} - {this.props.price} x {this.props.quantity}</p>
+
+            </div>
+        );
+    }
+}
+
+
+
+
+
 
 ReactDOM.render(
-  <Application />,
+  <ShoppingManager />,
   document.getElementById('app')
 );
